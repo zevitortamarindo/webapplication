c2d3cd636e691c7534478b3388f6de6eaf74aab7
zevitortamarindo <ze.v.tamarindo@gmail.com>
2022-05-27 21:04:24 +0000

    Atualiza para uma aplicação web usando Flask

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print('Olá Mundo')
+# Ao abrir o GitPod, execute:
+# pip install -r requirements.txt
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+app.run()
+
+
